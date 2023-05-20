a260684906cc93a7bb7edc0738e726879cab4646
Arpit Das <arpitdas118@gmail.com>
2023-05-20 23:13:18 +0530

    Basic started project

---
+++ src/test/java/com/learningspring/jdbcconnection/JdbcConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspring.jdbcconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
